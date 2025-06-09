d556e6d37f55903f3b956d7dae65f677aa157ee8
Rohan47 <152483882+RohanMuthyala47@users.noreply.github.com>
2025-06-09 13:47:00 +0530

    Delete VerilogCodes/Version 2.0 directory

--- VerilogCodes/Version 2.0/Subtractor.v
+++
@@ -1,9 +0,0 @@
-module Subtractor(
-    input [16:0] a,
-    output [16:0] diff
-);
-
-    parameter ONE = 17'b00000000010000000;
-    assign diff = ONE - a;
-
-endmodule
--- VerilogCodes/Version 2.0/multiplier_SRSC.v
+++
@@ -1,8 +0,0 @@
-module multiplier_SRSC(
-    input [8:0]p,
-    input[15:0]q,
-    
-    output [24:0]out_mul
-    );
-    assign out_mul=p*q;
-endmodule
