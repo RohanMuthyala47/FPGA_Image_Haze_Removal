9ca5e6b97fdd1327cf5e6fec8a4441d1e0279724
Rohan47 <rohanmuthyala1234@gmail.com>
2025-07-08 23:37:21 +0530

    Update Edge_Detection.v

--- Verilog/srcs/TE_Instantiations/Edge_Detection.v
+++ Verilog/srcs/TE_Instantiations/Edge_Detection.v
@@ -11,19 +11,19 @@ module ED(
     
     output wire [1:0] ED_out
 );
-    parameter D = 8'b01010000;
+    parameter D = 8'b01010000; // 80
     
-    wire  [7:0] diag1_diff = (in1 > in9) ? (in1 - in9) : (in9 - in1);
-    wire  [7:0] diag2_diff = (in3 > in7) ? (in3 - in7) : (in7 - in3);
-    wire  [7:0] horizontal_diff = (in4 > in6) ? (in4 - in6) : (in6 - in4);
-    wire  [7:0] vert_diff = (in2 > in8) ? (in2 - in8) : (in8 - in2);
+    wire [7:0] diag1_diff = (in1 > in9) ? (in1 - in9) : (in9 - in1);
+    wire [7:0] diag2_diff = (in3 > in7) ? (in3 - in7) : (in7 - in3);
+    wire [7:0] horizontal_diff = (in4 > in6) ? (in4 - in6) : (in6 - in4);
+    wire [7:0] vert_diff = (in2 > in8) ? (in2 - in8) : (in8 - in2);
     
     wire cond1 = (diag1_diff >= D) || (diag2_diff >= D);
     wire cond2 = (horizontal_diff >= D) || (vert_diff >= D);
     
 
     assign ED_out = cond1 ? 2'b10 : 
-                                 cond2 ? 2'b01 : 
-                                 2'b00;
+                    cond2 ? 2'b01 : 
+                    2'b00;
                            
 endmodule
