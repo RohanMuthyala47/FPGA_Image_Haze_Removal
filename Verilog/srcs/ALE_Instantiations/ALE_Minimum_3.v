08b59904e367343a37664ae9d0d572a622a7d944
Rohan47 <rohanmuthyala1234@gmail.com>
2025-07-09 21:57:37 +0530

    Update ALE_Minimum_3.v

--- Verilog/srcs/ALE_Instantiations/ALE_Minimum_3.v
+++ Verilog/srcs/ALE_Instantiations/ALE_Minimum_3.v
@@ -1,14 +1,14 @@
 // Find the dark channel
 module ALE_Minimum_3(
-    input  [7:0]   R,
-    input  [7:0]   G,
-    input  [7:0]   B,
+    input  [7:0] R,
+    input  [7:0] G,
+    input  [7:0] B,
     
     output [7:0] minimum
 );
 
     function [7:0] min;
-        input [7:0] x,y,z;
+        input [7:0] x, y, z;
         begin
             min = (x < y) ? ((x < z) ? x : z) : ((y < z) ? y : z);
         end
