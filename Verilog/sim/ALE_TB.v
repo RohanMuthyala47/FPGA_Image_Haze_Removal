72b0c171a048f44669b623b50fd85377c88bb1e6
Rohan47 <rohanmuthyala1234@gmail.com>
2025-07-09 20:17:57 +0530

    Update ALE_TB.v

--- Verilog/sim/ALE_TB.v
+++ Verilog/sim/ALE_TB.v
@@ -1,146 +1,148 @@
 `timescale 1ns/1ps
 
 module ALE_TB;
-reg clk;
-reg rst;
-reg [23:0]input_pixel;
-reg input_is_valid;
+    reg         clk;
+    reg         rst;
+    reg [23:0]  input_pixel;
+    reg         input_is_valid;
 
-wire [7:0] a_r;
-wire [7:0] a_g;
-wire [7:0] a_b;
-wire [15:0] inv_a_r;
-wire [15:0] inv_a_g;
-wire [15:0] inv_a_b;
-wire ale_valid;
+    wire [7:0]  a_r;
+    wire [7:0]  a_g;
+    wire [7:0]  a_b;
+    wire [15:0] inv_a_r;
+    wire [15:0] inv_a_g;
+    wire [15:0] inv_a_b;
+    wire        ale_valid;
 
-ale_top dut(
-clk,
-rst,
-input_pixel,
-input_is_valid,
+    ale_top dut (
+        .clk           (clk),
+        .rst           (rst),
+        .input_pixel   (input_pixel),
+        .input_is_valid(input_is_valid),
 
- a_r,
- a_g,
- a_b,
- inv_a_r,
- inv_a_g,
- inv_a_b,
- ale_valid
-);
+        .a_r           (a_r),
+        .a_g           (a_g),
+        .a_b           (a_b),
+        .inv_a_r       (inv_a_r),
+        .inv_a_g       (inv_a_g),
+        .inv_a_b       (inv_a_b),
+        .ale_valid     (ale_valid)
+    );
 
-   // Clock generation
-   initial clk = 1;
-   always #5 clk = ~clk;
+    // Clock generation
+    initial clk = 1;
+    always #5 clk = ~clk;
 
-   // File and image data
-   localparam array_length = 800*1024;
-    
-   reg [23:0] result[0:array_length-1];
-   reg [7:0] bmpdata[0:array_length-1];
-    
-   integer bmp_size, bmp_start_pos, bmp_width, bmp_height, bmp_count;
-   integer i, j;
-   // Task: Read BMP File
-   task read_file;
-       integer file1;
-       begin 
-           file1 = $fopen("canyon_512.bmp", "rb");
-           if (file1 == 0) begin
-               $display("Error: Cannot open BMP file.");
-               $finish;
-           end else begin
-               $fread(bmpdata, file1);
-               $fclose(file1);
+    // File and image data
+    localparam File_Size = 800*1024;
 
-               bmp_size       = {bmpdata[5], bmpdata[4], bmpdata[3], bmpdata[2]};
-               bmp_start_pos  = {bmpdata[13], bmpdata[12], bmpdata[11], bmpdata[10]};
-               bmp_width      = {bmpdata[21], bmpdata[20], bmpdata[19], bmpdata[18]};
-               bmp_height     = {bmpdata[25], bmpdata[24], bmpdata[23], bmpdata[22]};
-               bmp_count      = {bmpdata[29], bmpdata[28]};
+    reg [23:0] result[0:File_Size - 1];
+    reg [7:0]  bmpdata[0:File_Size - 1];
 
-               $display("BMP size         : %d", bmp_size);
-               $display("BMP start pos    : %d", bmp_start_pos);
-               $display("BMP width        : %d", bmp_width);
-               $display("BMP height       : %d", bmp_height);
-               $display("BMP bits/pixel   : %d", bmp_count);
+    integer bmp_size, bmp_start_pos, bmp_width, bmp_height, bmp_count;
+    integer i, j;
 
-               if (bmp_count != 24) begin
-                   $display("Error: BMP should be 24 bits/pixel.");
-                   $finish;
-               end
+    // Read from BMP File
+    task READ_FILE;
+        integer file1;
+        begin 
+            file1 = $fopen("canyon_512.bmp", "rb");
+            if (file1 == 0) begin
+                $display("Error: Cannot open BMP file.");
+                $finish;
+            end else begin
+                $fread(bmpdata, file1);
+                $fclose(file1);
 
-               if (bmp_width % 4) begin
-                   $display("Error: BMP width must be divisible by 4.");
-                   $finish;
-               end
-           end
-       end
-   endtask
-    
-   //write BMP file
-   task write_file;
-       integer file2,i;
-       begin
-       file2 = $fopen("output_file.bmp","wb");
-        
-       for(i = 0;i < bmp_start_pos; i = i + 1)
-       begin   
-               $fwrite(file2, "%c", bmpdata[i]);
-       end
-        
-        for(i = bmp_start_pos;i < bmp_size; i = i + 1)
-              begin   
-                      $fwrite(file2, "%c%c%c",result [i-bmp_start_pos][7:0],
-                                                                   result [i-bmp_start_pos][15:8],                                           
-                                                                   result [i-bmp_start_pos][23:16]);
-                       
-              end
-        
-       $fclose(file2);
-       $display("write successful");
-        
-       end
-   endtask
+                bmp_size       = {bmpdata[5], bmpdata[4], bmpdata[3], bmpdata[2]};
+                bmp_start_pos  = {bmpdata[13], bmpdata[12], bmpdata[11], bmpdata[10]};
+                bmp_width      = {bmpdata[21], bmpdata[20], bmpdata[19], bmpdata[18]};
+                bmp_height     = {bmpdata[25], bmpdata[24], bmpdata[23], bmpdata[22]};
+                bmp_count      = {bmpdata[29], bmpdata[28]};
 
-   initial begin
-       rst = 1;
-       input_is_valid = 0;
-       input_pixel=0;
-       read_file;
-       #10;
-       rst = 0;
+                $display("BMP size         : %d", bmp_size);
+                $display("BMP start pos    : %d", bmp_start_pos);
+                $display("BMP width        : %d", bmp_width);
+                $display("BMP height       : %d", bmp_height);
+                $display("BMP bits/pixel   : %d", bmp_count);
 
-       for (i = bmp_start_pos; i < bmp_size; i = i + 3) begin
-           input_pixel[7:0]  = bmpdata[i];
-           input_pixel[15:8] = bmpdata[i + 1];
-           input_pixel[23:16]   = bmpdata[i + 2];
-           #10;
-          input_is_valid = 1;
-       end
-        
-        
-       #10;
-       input_is_valid = 0;
-       #10;
-       write_file;
-       #10;
-       $stop;
-   end
+                if (bmp_count != 24) begin
+                    $display("Error: BMP should be 24 bits/pixel.");
+                    $finish;
+                end
 
-   // Output Monitor
-   always @(posedge clk) begin
-       if (rst)
-           j <= 0;
-       else if (ale_valid) begin
-           $display("Output Pixel %0d:", j);
-           $display("a_r: %d, a_g: %d, a_b: %d", a_r, a_g, a_b);
-           $display("inv_a_r: %d, inv_a_g: %d, inv_a_b: %d", inv_a_r, inv_a_g, inv_a_b);
-           j <= j+1;
-       end
-   end
+                if (bmp_width % 4) begin
+                    $display("Error: BMP width must be divisible by 4.");
+                    $finish;
+                end
+            end
+        end
+    endtask
+
+    // Write to BMP file
+    task WRITE_FILE;
+        integer file2, i;
+        begin
+            file2 = $fopen("output_file.bmp", "wb");
+
+            for (i = 0; i < bmp_start_pos; i = i + 1) begin   
+                $fwrite(file2, "%c", bmpdata[i]);
+            end
+
+            for (i = bmp_start_pos; i < bmp_size; i = i + 1) begin   
+                $fwrite(file2, "%c%c%c", 
+                    result[i - bmp_start_pos][7:0],
+                    result[i - bmp_start_pos][15:8],                                           
+                    result[i - bmp_start_pos][23:16]);
+            end
+
+            $fclose(file2);
+            $display("write successful");
+        end
+    endtask
+
+    initial begin
+        rst = 1;
+        input_is_valid = 0;
+        input_pixel = 0;
+     
+        READ_FILE;
+     
+        #10;
+        rst = 0;
+
+        for (i = bmp_start_pos; i < bmp_size; i = i + 3) begin
+            input_pixel[7:0]    = bmpdata[i];
+            input_pixel[15:8]   = bmpdata[i + 1];
+            input_pixel[23:16]  = bmpdata[i + 2];
+            #10;
+            input_is_valid = 1;
+        end
+
+        #10;
+        input_is_valid = 0;
+     
+        #10;
+        WRITE_FILE;
+     
+        #10;
+        $stop;
+    end
+
+    // Output Monitor
+    always @(posedge clk) begin
+        if (rst)
+            j <= 0;
+        else if (ale_valid) begin
+            $display("Output Pixel %0d:", j);
+            $display("A_R: %d, A_G: %d, A_B: %d", a_r, a_g, a_b);
+            $display("Inv_AR: %d, Inv_AG: %d, Inv_AB: %d", inv_a_r, inv_a_g, inv_a_b);
+            j <= j + 1;
+        end
+    end
 
 endmodule
+
 // `timescale 1ns/1ps
 
 // module ale_tb;
