99b32be89962b14e88e3febda1b73644c9dc1c11
Rohan47 <rohanmuthyala1234@gmail.com>
2025-07-09 22:14:14 +0530

    Update ALE_TE_Top.v

--- Verilog/srcs/Top_Modules/ALE_TE_Top.v
+++ Verilog/srcs/Top_Modules/ALE_TE_Top.v
@@ -49,6 +49,7 @@ module ALE_TE_Top(
     ALE ALE (
         .clk(clk),
         .rst(reset),
+        
         .input_is_valid(ale_enable),
         .input_pixel_1(window_pixel_1),
         .input_pixel_2(window_pixel_2),
@@ -59,12 +60,14 @@ module ALE_TE_Top(
         .input_pixel_7(window_pixel_7),
         .input_pixel_8(window_pixel_8),
         .input_pixel_9(window_pixel_9),
+        
         .A_R(atmospheric_light_red),
         .A_G(atmospheric_light_green),
         .A_B(atmospheric_light_blue),
         .Inv_A_R(inverse_atm_red),
         .Inv_A_G(inverse_atm_green),
         .Inv_A_B(inverse_atm_blue),
+        
         .output_is_valid(ale_output_valid),
         .done(ale_processing_done)
     );
@@ -80,12 +83,12 @@ module ALE_TE_Top(
             latched_inverse_atm_red   <= 16'b0;
             latched_inverse_atm_green <= 16'b0;
             latched_inverse_atm_blue  <= 16'b0;
-            atmospheric_data_ready    <= 1'b0;
+            
         end else if (ale_processing_done) begin
             latched_inverse_atm_red   <= inverse_atm_red;
             latched_inverse_atm_green <= inverse_atm_green;
             latched_inverse_atm_blue  <= inverse_atm_blue;
-            atmospheric_data_ready    <= 1'b1;
+            
         end
     end
 
@@ -98,14 +101,22 @@ module ALE_TE_Top(
     TE TE (
         .clk(clk),
         .rst(reset),
+        
         .input_is_valid(te_enable),
-        .in1(window_pixel_1), .in2(window_pixel_2), .in3(window_pixel_3),
-        .in4(window_pixel_4), .in5(window_pixel_5), .in6(window_pixel_6),
-        .in7(window_pixel_7), .in8(window_pixel_8), .in9(window_pixel_9),
+        .in1(window_pixel_1),
+        .in2(window_pixel_2),
+        .in3(window_pixel_3),
+        .in4(window_pixel_4),
+        .in5(window_pixel_5),
+        .in6(window_pixel_6),
+        .in7(window_pixel_7),
+        .in8(window_pixel_8),
+        .in9(window_pixel_9),
+        
         .inv_ar(latched_inverse_atm_red),
         .inv_ag(latched_inverse_atm_green),
         .inv_ab(latched_inverse_atm_blue),
-        .atm_valid(atmospheric_data_ready),
+        
         .transmission(calculated_transmission),
         .output_is_valid(te_output_valid)
     );
