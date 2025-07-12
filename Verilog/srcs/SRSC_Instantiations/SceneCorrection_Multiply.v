e0f8123ad13f15c52c8f3dc30b28975baa0f1b7d
Rohan47 <rohanmuthyala1234@gmail.com>
2025-07-12 19:03:03 +0530

    Add files via upload

---
+++ Verilog/srcs/SRSC_Instantiations/SceneCorrection_Multiply.v
@@ -0,0 +1,15 @@
+module Saturation_Correction_Multiplier (
+    input  [7:0]  x1,          // For x^0.3
+    input  [7:0]  x2,          // For x^0.7
+    output [7:0] result    // Final unsigned 8-bit result
+);
+
+    // Q8.8 * Q8.8 = Q16.16
+    wire [31:0] mult_result = x1 * x2;
+
+    // Convert Q16.16 to Q8.8
+    wire [15:0] q8_8_result = mult_result[23:8];
+
+    assign result = (q8_8_result[15:8] > 8'd255) ? 8'd255 : q8_8_result[15:8];
+
+endmodule
