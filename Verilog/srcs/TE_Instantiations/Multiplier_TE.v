0a058dcfc166dd9a48a416fa2cb4cbe05923a2fd
Rohan47 <rohanmuthyala1234@gmail.com>
2025-07-15 00:15:49 +0530

    Update and rename Multiplier.v to Multiplier_TE.v

---
+++ Verilog/srcs/TE_Instantiations/Multiplier_TE.v
@@ -0,0 +1,26 @@
+module Multiplier (
+    input  [15:0] a,      // Inverted Atmospheric Light value in Q0.16 format
+    input  [7:0]  b,      // Edge Detection Filter result
+    output [15:0] product // Product in Q0.16 format
+);
+
+    parameter [15:0] OMEGA       = 16'd61440; // 0.9375 in Q0.16
+    parameter [15:0] UPPER_BOUND = 16'd49152; // 0.75 in Q0.16
+
+    // Unscaled result is in Q8.16 format
+    wire [23:0] unscaled_product = a * b;
+
+    // Scale the result with OMEGA (Q0.16)
+    // Scaled result is in Q8.32 format
+    wire [39:0] scaled_product = unscaled_product * OMEGA;
+
+    // Trim the product down to Q0.16 format
+    wire [15:0] result = scaled_product[31:16];
+
+    // Check if the unscaled product is greater than 1 to prevent roll around due to negative result in the Subtractor circuit
+    wire is_gt_one = (unscaled_product[23:16] != 0);
+
+    // Clamp output to 0.75 unscaled product is greater than 1
+    assign product = is_gt_one ? UPPER_BOUND : result;
+
+endmodule
