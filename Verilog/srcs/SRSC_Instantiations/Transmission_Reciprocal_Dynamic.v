0cf3acd0f5ba8788915b0d740b04c362b8fac035
Rohan47 <rohanmuthyala1234@gmail.com>
2025-07-15 20:12:45 +0530

    Update Transmission_Reciprocal_Dynamic.v

--- Verilog/srcs/SRSC_Instantiations/Transmission_Reciprocal_Dynamic.v
+++ Verilog/srcs/SRSC_Instantiations/Transmission_Reciprocal_Dynamic.v
@@ -2,6 +2,7 @@ module Trans_LUT (
     input  [15:0] x,     // Q0.16 Transmission value input (unsigned)
     output [15:0] y      // Q2.14 Inverse Transmission value output (unsigned)
 );
+    
     wire [31:0] reciprocal;
     assign reciprocal = (x != 0) ? (32'd1073741824 / x) : 32'd65535;
     
