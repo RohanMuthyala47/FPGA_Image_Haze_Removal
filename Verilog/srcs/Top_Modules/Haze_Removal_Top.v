cb6cd15fbbe4b104631ce175fa9438e98f5d6d40
Rohan47 <rohanmuthyala1234@gmail.com>
2025-07-18 19:46:30 +0530

    Update Haze_Removal_Top.v

--- Verilog/srcs/Top_Modules/Haze_Removal_Top.v
+++ Verilog/srcs/Top_Modules/Haze_Removal_Top.v
@@ -1,4 +1,4 @@
-module Haze_Removal_Top (
+module Haze_Removal_Top(
 
     // AXI4-Stream Global Signals
     input         ACLK,
@@ -19,9 +19,9 @@ module Haze_Removal_Top (
     output        M_AXIS_TLAST,
     input         M_AXIS_TREADY
 );
-
-    assign S_AXIS_TREADY = 1'b1; // Always ready to accept data
+    wire ALE_clk, TE_SRSC_clk;
     
+    assign S_AXIS_TREADY = 1'b1; // Always ready to accept data
     assign M_AXIS_TLAST = 1'b0; // Continuous stream
 
     // 3x3 Window of RGB pixels generated from Line Buffers
@@ -48,16 +48,16 @@ module Haze_Removal_Top (
     
     wire [7:0] A_R, A_G, A_B;
     wire [15:0] Inv_AR, Inv_AG, Inv_AB;
+    
     wire ALE_done;
-
-    wire ALE_enable = (~enable) & window_valid;
+    wire ALE_enable = ~ALE_done & enable;
 
     // Atmospheric Light Estimation
     ALE ALE (
-        .clk(ACLK),
+        .clk(ALE_clk),
         .rst(~ARESETn),
         
-        .input_is_valid(ALE_enable),
+        .input_is_valid(window_valid & ALE_enable),
         .input_pixel_1(Pixel_00), .input_pixel_2(Pixel_01), .input_pixel_3(Pixel_02),
         .input_pixel_4(Pixel_10), .input_pixel_5(Pixel_11), .input_pixel_6(Pixel_12),
         .input_pixel_7(Pixel_20), .input_pixel_8(Pixel_21), .input_pixel_9(Pixel_22),
@@ -88,14 +88,15 @@ module Haze_Removal_Top (
     wire [7:0] J_R, J_G, J_B;
     assign M_AXIS_TDATA = {8'h00, J_R, J_G, J_B};
 
-    wire TE_SRSC_enable = enable & window_valid;
+    wire TE_SRSC_enable = ALE_done & enable;
+    wire TE_SRSC_done;
     
     // Transmission Estimation, Scene Recovery and Saturation Correction
     TE_and_SRSC TE_SRSC (
-        .clk(ACLK),
+        .clk(TE_SRSC_clk),
         .rst(~ARESETn),
         
-        .input_is_valid(TE_SRSC_enable),
+        .input_is_valid(window_valid & TE_SRSC_enable),
         .in1(Pixel_00), .in2(Pixel_01), .in3(Pixel_02),
         .in4(Pixel_10), .in5(Pixel_11), .in6(Pixel_12),
         .in7(Pixel_20), .in8(Pixel_21), .in9(Pixel_22),
@@ -105,8 +106,29 @@ module Haze_Removal_Top (
         .Inv_AR(Final_Inv_AR), .Inv_AG(Final_Inv_AG), .Inv_AB(Final_Inv_AB),
         
         .J_R(J_R), .J_G(J_G), .J_B(J_B),
+        .output_valid(M_AXIS_TVALID),
         
-        .output_valid(M_AXIS_TVALID)
+        .done(TE_SRSC_done)
+    );
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    // Clock Gating Cells for ALE AND TE_SRSC
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+    Clock_Gating_Cell ALE_CGC(
+        .clk(ACLK),
+        .clk_enable(ALE_enable),
+        .rstn(~ARESETn),
+        
+        .clk_gated(ALE_clk)
+    );
+    
+    Clock_Gating_Cell TE_SRSC_CGC(
+        .clk(ACLK),
+        .clk_enable(TE_SRSC_enable),
+        .rstn(~ARESETn),
+        
+        .clk_gated(TE_SRSC_clk)
     );
 
 endmodule
