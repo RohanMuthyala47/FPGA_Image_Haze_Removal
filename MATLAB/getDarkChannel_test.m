0cdf1163e6bf11e949904dec3488ae8009f075bc
Rohan47 <rohanmuthyala1234@gmail.com>
2025-07-29 19:21:54 +0530

    Delete MATLAB/getDarkChannel_test.m

--- MATLAB/getDarkChannel_test.m
+++
@@ -1,4 +0,0 @@
-hazyImg = im2double(imread("C:\Users\Rohan\Downloads\canyon.jpg"));
-patchSize = 3;
-darkChannel = getDarkChannel(hazyImg, patchSize);
-figure, imshow(darkChannel), title('Dark Channel');
