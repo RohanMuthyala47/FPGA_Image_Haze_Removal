d2a6739258981388c0102d7cab1bad9f634c3faf
Rohan47 <rohanmuthyala1234@gmail.com>
2025-08-06 22:33:28 +0530

    Update Block_P1.v

--- Vivado/Verilog/sources/TE_Instantiations/Block_P1.v
+++ Vivado/Verilog/sources/TE_Instantiations/Block_P1.v
@@ -1,4 +1,5 @@
-module Block_P1(
+// Edge Preserving Filter for Vertical and Horizontal edges
+module Block_P1 (
     input [7:0]  in1,
     input [7:0]  in2,
     input [7:0]  in3,
@@ -12,8 +13,8 @@ module Block_P1(
     output [7:0] p1_result
 );
     
-    wire [15:0] sum;
-    assign sum = in1 + (in2 << 1) + in3 + (in4 << 1) + (in5 << 2) + (in6 << 1) + in7 + (in8 << 1) + in9;
+    wire [11:0] sum;
+    assign sum = (in1) + (in2 << 1) + (in3) + (in4 << 1) + (in5 << 2) + (in6 << 1) + (in7) + (in8 << 1) + (in9);
     
     assign p1_result = sum >> 4;
     
