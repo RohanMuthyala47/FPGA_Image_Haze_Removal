c20bd407201e30436658b7a88a5e12ec9c968101
Rohan47 <rohanmuthyala1234@gmail.com>
2025-08-09 15:40:14 +0530

    Update Multiplier_TE.v

--- Vivado/Verilog/sources/TE_Instantiations/Multiplier_TE.v
+++ Vivado/Verilog/sources/TE_Instantiations/Multiplier_TE.v
@@ -1,16 +1,14 @@
 // Compute ω * Pc / Ac
 module Multiplier (
-    input clk,
-    input rst,
+    input         clk,
+    input         rst,
     
     input  [15:0] Ac_Inv, // Scaled Inverted Atmospheric Light value in Q0.16 format (ω * 1/Ac) 
     input  [7:0]  Pc,     // Edge Detection Filter result
     
     output [15:0] product // ω * min(Pc / Ac) ; c ∈ {R, G, B} in Q0.16 format
 );
-
-    parameter [15:0] MAX_OUTPUT = 16'd47415; // 0.725 in Q0.16
-
+    
     // Pipeline registers
     reg [15:0] Ac_Inv_P;
     reg [7:0]  Pc_P;
@@ -29,9 +27,6 @@ module Multiplier (
     
     wire [23:0] result = Ac_Inv_P * Pc_P; // Q8.16
     
-    // Overflow detection
-    wire is_gt_one = (result[23:16] != 0);
-    
-    assign product = is_gt_one ? MAX_OUTPUT : result[15:0];
+    assign product = result[15:0]; // Scale down to Q0.16 and eliminate overflow
     
 endmodule
