3fd5e78775e9f9cee245abcdf055e3d9fe4c95cf
Rohan47 <rohanmuthyala1234@gmail.com>
2025-08-10 12:02:20 +0530

    Update and rename SceneCorrection_Multiply.v to Saturation_Correction_Multiplier.v

---
+++ Vivado/Verilog/sources/SRSC_Instantiations/Saturation_Correction_Multiplier.v
@@ -0,0 +1,15 @@
+module Saturation_Correction_Multiplier (
+    input  [15:0] x1,       // Q3.13 format
+    input  [15:0] x2,       // Q6.10 format
+    output [7:0]  result    // Unsigned 8-bit output
+);
+    // Q3.13 * Q6.10 = Q9.23
+    wire [31:0] mult_result = x1 * x2;
+    
+    // Trim off fractional bits
+    wire [8:0] int_val = mult_result[31:23];
+    
+    // Scale down to 8 bit value
+    assign result = (int_val > 9'd255) ? 8'd255 : int_val[7:0];
+    
+endmodule
