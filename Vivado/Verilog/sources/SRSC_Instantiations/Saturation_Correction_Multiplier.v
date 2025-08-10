a97771c695da892655712a872a3e9a2ec13a75ee
Rohan47 <rohanmuthyala1234@gmail.com>
2025-08-10 12:03:29 +0530

    Update Saturation_Correction_Multiplier.v

--- Vivado/Verilog/sources/SRSC_Instantiations/Saturation_Correction_Multiplier.v
+++ Vivado/Verilog/sources/SRSC_Instantiations/Saturation_Correction_Multiplier.v
@@ -1,8 +1,10 @@
+// Compute Ac^β * Jc^(1-β)
 module Saturation_Correction_Multiplier (
     input  [15:0] x1,       // Q3.13 format
     input  [15:0] x2,       // Q6.10 format
-    output [7:0]  result    // Unsigned 8-bit output
+    output  [7:0] result    // Unsigned 8-bit output
 );
+    
     // Q3.13 * Q6.10 = Q9.23
     wire [31:0] mult_result = x1 * x2;
     
