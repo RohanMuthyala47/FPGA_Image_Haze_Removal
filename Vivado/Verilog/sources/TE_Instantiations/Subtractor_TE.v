266c77c06ba6148b096ebfb524f0e4d45e8de85f
Rohan47 <rohanmuthyala1234@gmail.com>
2025-08-10 18:40:00 +0530

    Update Subtractor_TE.v

--- Vivado/Verilog/sources/TE_Instantiations/Subtractor_TE.v
+++ Vivado/Verilog/sources/TE_Instantiations/Subtractor_TE.v
@@ -5,9 +5,9 @@ module Subtractor (
     output [15:0] out // Transmission value in Q0.16
 );
     
-    parameter [15:0] ONE    = 16'd65535; // 0.999... = 1.0 in Q0.16 format
-    parameter [15:0] T0     = 16'd19661; // Lower bound for transmission = 0.3 in Q0.16 format
-    parameter [15:0] MAX_T  = 16'd45875; // ONE - T0 = 0.7 in Q0.16 format
+    parameter [15:0] ONE = 16'd65535; // 0.999.... = 1.0 in Q0.16 format
+    parameter [15:0] T0  = 16'd21299; // Lower bound for transmission = 0.325 in Q0.16 format
+    parameter [15:0] MAX_T  = 16'd44237; // ONE - T0 = 0.675 in Q0.16 format
     
     assign out = (in > MAX_T) ?  T0 : ONE - in;
     
