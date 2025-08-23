8c1802e017edffc7ff67b57abedc24af951bba31
Rohan47 <rohanmuthyala1234@gmail.com>
2025-08-23 21:38:02 +0530

    Update Edge_Detection_Top.v

--- Vivado/Verilog/sources/TE_Instantiations/Edge_Detection_Top.v
+++ Vivado/Verilog/sources/TE_Instantiations/Edge_Detection_Top.v
@@ -1,56 +1,32 @@
 // Detect the type of edge in a given 3x3 window of RGB Pixels : Diagonal, Horizontal, Vertical or no edge
 module ED_Top (
-    input [23:0] input_pixel_1,
-    input [23:0] input_pixel_2,
-    input [23:0] input_pixel_3,
-    input [23:0] input_pixel_4,
-
-    input [23:0] input_pixel_6,
-    input [23:0] input_pixel_7,
-    input [23:0] input_pixel_8,
-    input [23:0] input_pixel_9,
+    input [23:0] input_pixel_1, input_pixel_2, input_pixel_3,
+                 input_pixel_4,                input_pixel_6,
+                 input_pixel_7, input_pixel_8, input_pixel_9,
     
     output [1:0] ED1_out, ED2_out, ED3_out
 );
     
-    ED Red_Edge_Detection (
-        .input_pixel_1(input_pixel_1[23:16]),
-        .input_pixel_2(input_pixel_2[23:16]),
-        .input_pixel_3(input_pixel_3[23:16]),
-        .input_pixel_4(input_pixel_4[23:16]),
-
-        .input_pixel_6(input_pixel_6[23:16]),
-        .input_pixel_7(input_pixel_7[23:16]),
-        .input_pixel_8(input_pixel_8[23:16]),
-        .input_pixel_9(input_pixel_9[23:16]),
+    ED Edge_Detection_Red (
+        .input_pixel_1(input_pixel_1[23:16]), .input_pixel_2(input_pixel_2[23:16]), .input_pixel_3(input_pixel_3[23:16]),
+        .input_pixel_4(input_pixel_4[23:16]),                                       .input_pixel_6(input_pixel_6[23:16]),
+        .input_pixel_7(input_pixel_7[23:16]), .input_pixel_8(input_pixel_8[23:16]), .input_pixel_9(input_pixel_9[23:16]),
         
         .ED_out(ED1_out)
     );
         
-    ED Green_Edge_Detection (
-        .input_pixel_1(input_pixel_1[15:8]),
-        .input_pixel_2(input_pixel_2[15:8]),
-        .input_pixel_3(input_pixel_3[15:8]),
-        .input_pixel_4(input_pixel_4[15:8]),
-
-        .input_pixel_6(input_pixel_6[15:8]),
-        .input_pixel_7(input_pixel_7[15:8]),
-        .input_pixel_8(input_pixel_8[15:8]),
-        .input_pixel_9(input_pixel_9[15:8]),
+    ED Edge_Detection_Green (
+        .input_pixel_1(input_pixel_1[15:8]), .input_pixel_2(input_pixel_2[15:8]), .input_pixel_3(input_pixel_3[15:8]),
+        .input_pixel_4(input_pixel_4[15:8]),                                      .input_pixel_6(input_pixel_6[15:8]),
+        .input_pixel_7(input_pixel_7[15:8]), .input_pixel_8(input_pixel_8[15:8]), .input_pixel_9(input_pixel_9[15:8]),
         
         .ED_out(ED2_out)
     );
         
-    ED Blue_Edge_Detection (
-        .input_pixel_1(input_pixel_1[7:0]),
-        .input_pixel_2(input_pixel_2[7:0]),
-        .input_pixel_3(input_pixel_3[7:0]),
-        .input_pixel_4(input_pixel_4[7:0]),
-
-        .input_pixel_6(input_pixel_6[7:0]),
-        .input_pixel_7(input_pixel_7[7:0]),
-        .input_pixel_8(input_pixel_8[7:0]),
-        .input_pixel_9(input_pixel_9[7:0]),
+    ED Edge_Detection_Blue (
+        .input_pixel_1(input_pixel_1[7:0]), .input_pixel_2(input_pixel_2[7:0]), .input_pixel_3(input_pixel_3[7:0]),
+        .input_pixel_4(input_pixel_4[7:0]),                                     .input_pixel_6(input_pixel_6[7:0]),
+        .input_pixel_7(input_pixel_7[7:0]), .input_pixel_8(input_pixel_8[7:0]), .input_pixel_9(input_pixel_9[7:0]),
         
         .ED_out(ED3_out)
     );
