c093fa4fb53845c5f76daf1d31fb6402f3fed62c
Rohan47 <rohanmuthyala1234@gmail.com>
2025-08-23 21:36:13 +0530

    Update Block_P2.v

--- Vivado/Verilog/sources/TE_Instantiations/Block_P2.v
+++ Vivado/Verilog/sources/TE_Instantiations/Block_P2.v
@@ -1,22 +1,16 @@
 // Edge Preserving Filter applied when Diagonal Edges are detected
 module Block_P2 (
-    input clk, rst,
+    input        clk, rst,
     
-    input  [7:0] in1,
-    input  [7:0] in2,
-    input  [7:0] in3,
-    input  [7:0] in4,
-    input  [7:0] in5,
-    input  [7:0] in6,
-    input  [7:0] in7,
-    input  [7:0] in8,
-    input  [7:0] in9,
+    input  [7:0] in1, in2, in3,
+                 in4, in5, in6,
+                 in7, in8, in9,
     
     output [7:0] p2_result
 );
     
     // Pipeline Registers
-    reg [7:0]   in1_P, in2_P, in3_P, 
+    reg   [7:0] in1_P, in2_P, in3_P, 
                 in4_P, in5_P, in6_P, 
                 in7_P, in8_P, in9_P;
     
