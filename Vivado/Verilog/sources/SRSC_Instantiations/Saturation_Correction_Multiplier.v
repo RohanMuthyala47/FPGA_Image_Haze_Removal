79ff5409e513ce505a8ce038f0945d39e5ee80ef
Rohan47 <rohanmuthyala1234@gmail.com>
2025-08-28 22:13:11 +0530

    Update Saturation_Correction_Multiplier.v

--- Vivado/Verilog/sources/SRSC_Instantiations/Saturation_Correction_Multiplier.v
+++ Vivado/Verilog/sources/SRSC_Instantiations/Saturation_Correction_Multiplier.v
@@ -1,21 +1,23 @@
 // Compute Ac^β * Jc^(1-β)
 module Saturation_Correction_Multiplier (
-    input clk, rst, 
-    input  [15:0] x1,       // Q3.13 format
-    input  [15:0] x2,       // Q6.10 format
-    output  [7:0] result    // Unsigned 8-bit output
+    input         clk, rst, 
+    input  [11:0] x1,       // Q3.9 format
+    input  [11:0] x2,       // Q6.6 format
+    output [7:0]  result    // *-bit result
 );
     
-    reg [31:0] product;
+    reg [11:0] x1_P, x2_P;
+    
+    wire [23:0] product;
     
     always @(posedge clk) begin
-        if(rst)
-            product <= 0;
-        else
-            product <= x1 * x2;
+            x1_P <= x1;
+            x2_P <= x2;
     end
     
+    assign product = x1_P * x2_P; // Q9.15
+    
     // Scale down to 8 bit value
-    assign result = product[30:23];
+    assign result = (product[23:15] > 8'd255) ? 8'd255 : product[22:15];
     
 endmodule
