581a0da7475a1ab8affc2a4ef6366bd1261846d4
Rohan47 <rohanmuthyala1234@gmail.com>
2025-08-28 22:19:54 +0530

    Update Stage_7_Mux.v

--- Vivado/Verilog/sources/TE_Instantiations/Stage_7_Mux.v
+++ Vivado/Verilog/sources/TE_Instantiations/Stage_7_Mux.v
@@ -1,14 +1,14 @@
 // Stage 7 multiplexer to choose the pre-transmission value based on edge detected
-module Stage_7_Mux (
-    input  [15:0] a, b, c,
+module Pre_Transmission_Mux (
+    input  [13:0] a, b, c,
     input  [1:0]  sel,
         
-    output [15:0] out
+    output [13:0] out
 );
     
     assign out = (sel == 2'b00) ? a : 
                  (sel == 2'b01) ? b : 
                  (sel == 2'b10) ? c :
-                 'b0;
+                 13'b0;
     
 endmodule
