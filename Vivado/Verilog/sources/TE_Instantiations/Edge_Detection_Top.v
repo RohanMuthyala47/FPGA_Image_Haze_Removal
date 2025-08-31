59c181e9f79075e89ecb8e05c3424f40bef8337a
Rohan47 <rohanmuthyala1234@gmail.com>
2025-08-31 18:44:07 +0530

    Update Edge_Detection_Top.v

--- Vivado/Verilog/sources/TE_Instantiations/Edge_Detection_Top.v
+++ Vivado/Verilog/sources/TE_Instantiations/Edge_Detection_Top.v
@@ -1,5 +1,6 @@
-// Detect the type of edge in a given 3x3 window of RGB Pixels : Diagonal, Horizontal, Vertical or no edge
+// Detect the type of edge in a given 3x3 window of RGB Pixels : Diagonal, Horizontal, Vertical or No edge
 module ED_Top (
+    input input_valid,
     input [23:0] input_pixel_1, input_pixel_2, input_pixel_3,
                  input_pixel_4,                input_pixel_6,
                  input_pixel_7, input_pixel_8, input_pixel_9,
@@ -7,7 +8,8 @@ module ED_Top (
     output [1:0] ED1_out, ED2_out, ED3_out
 );
     
-    ED Edge_Detection_Red (
+    ED Red_Edge_Detection (
+        .input_valid(input_valid),
         .input_pixel_1(input_pixel_1[23:16]), .input_pixel_2(input_pixel_2[23:16]), .input_pixel_3(input_pixel_3[23:16]),
         .input_pixel_4(input_pixel_4[23:16]),                                       .input_pixel_6(input_pixel_6[23:16]),
         .input_pixel_7(input_pixel_7[23:16]), .input_pixel_8(input_pixel_8[23:16]), .input_pixel_9(input_pixel_9[23:16]),
@@ -15,7 +17,8 @@ module ED_Top (
         .ED_out(ED1_out)
     );
         
-    ED Edge_Detection_Green (
+    ED Green_Edge_Detection (
+        .input_valid(input_valid),
         .input_pixel_1(input_pixel_1[15:8]), .input_pixel_2(input_pixel_2[15:8]), .input_pixel_3(input_pixel_3[15:8]),
         .input_pixel_4(input_pixel_4[15:8]),                                      .input_pixel_6(input_pixel_6[15:8]),
         .input_pixel_7(input_pixel_7[15:8]), .input_pixel_8(input_pixel_8[15:8]), .input_pixel_9(input_pixel_9[15:8]),
@@ -23,7 +26,8 @@ module ED_Top (
         .ED_out(ED2_out)
     );
         
-    ED Edge_Detection_Blue (
+    ED Blue_Edge_Detection (
+    .input_valid(input_valid),
         .input_pixel_1(input_pixel_1[7:0]), .input_pixel_2(input_pixel_2[7:0]), .input_pixel_3(input_pixel_3[7:0]),
         .input_pixel_4(input_pixel_4[7:0]),                                     .input_pixel_6(input_pixel_6[7:0]),
         .input_pixel_7(input_pixel_7[7:0]), .input_pixel_8(input_pixel_8[7:0]), .input_pixel_9(input_pixel_9[7:0]),
