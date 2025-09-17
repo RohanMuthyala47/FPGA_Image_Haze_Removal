97b6f17fe1899131a5576688b12c9d76de07deba
Rohan47 <rohanmuthyala1234@gmail.com>
2025-09-17 14:02:07 +0530

    Update Subtractor_TE.v

--- Vivado/RTL/sources/TE_Instantiations/Subtractor_TE.v
+++ Vivado/RTL/sources/TE_Instantiations/Subtractor_TE.v
@@ -6,8 +6,8 @@ module Subtractor_TE (
 );
     
     parameter [13:0] ONE = 16'd16383; // 0.999.... = 1.0 in Q0.14 format
-    parameter [13:0] T0  = 16'd5734; // Lower bound for transmission = 0.325 in Q0.14 format
-    parameter [13:0] MAX_T  = ONE - T0; // ONE - T0 = 0.675 in Q0.14 format
+    parameter [13:0] T0  = 16'd5734; // Lower bound for transmission = 0.35 in Q0.14 format
+    parameter [13:0] MAX_T  = ONE - T0; // ONE - T0 = 0.65 in Q0.14 format
     
     assign out = (in > MAX_T) ?  T0 : ONE - in;
     
