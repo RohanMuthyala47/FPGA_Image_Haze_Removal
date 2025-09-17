b13b728ffb55f71713568a4e3a683d804303d120
Rohan47 <rohanmuthyala1234@gmail.com>
2025-09-17 14:01:30 +0530

    Update Multiplier_TE.v

--- Vivado/RTL/sources/TE_Instantiations/Multiplier_TE.v
+++ Vivado/RTL/sources/TE_Instantiations/Multiplier_TE.v
@@ -1,22 +1,24 @@
 // Compute ω * Pc / Ac
 module Multiplier_TE (
-    input         clk, rst,
+    input         clk,
+    input         rst,
     
-    input  [15:0] Ac_Inv, // Scaled Inverted Atmospheric Light value in Q0.16 format (ω * 1/Ac) 
-    input  [7:0]  Pc,     // Edge Detection Filter result
+    input  [7:0]  Fc,     // Filter result
+    input  [15:0] Inv_Ac, // Scaled Inverted Atmospheric Light value in Q0.16 format (ω * 1/Ac) 
     
-    output [13:0] product // ω * min(Pc / Ac) ; c ∈ {R, G, B} in Q0.16 format
+    output [13:0] product // ω * min(Pc / Ac) ; c ? {R, G, B} in Q0.14 format
 );
     
-    // Pipeline register
+    // Pipeline registers
     reg [21:0] result_P;
     
+    // Pipeline inputs to reduce fan-out
     always @(posedge clk) begin
         if(rst) begin
             result_P <= 0;
         end
         else begin
-            result_P <= Pc * Ac_Inv[15:2]; // Q8.14
+            result_P <= Fc * Inv_Ac[15:2]; // Q8.14
         end
     end
     
