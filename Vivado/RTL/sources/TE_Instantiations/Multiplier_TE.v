1cfa44e7f6e3b8a5415305fe8d66547321b821c5
Rohan47 <rohanmuthyala1234@gmail.com>
2025-09-21 21:22:15 +0530

    Update Multiplier_TE.v

--- Vivado/RTL/sources/TE_Instantiations/Multiplier_TE.v
+++ Vivado/RTL/sources/TE_Instantiations/Multiplier_TE.v
@@ -6,7 +6,7 @@ module Multiplier_TE (
     input  [7:0]  Fc,     // Filter result
     input  [13:0] Inv_Ac, // Scaled Inverted Atmospheric Light value in Q0.14 format (ω * 1/Ac) 
     
-    output [13:0] product // ω * min(Pc / Ac) ; c ∈ {R, G, B} in Q0.14 format
+    output [9:0] product // ω * min(Pc / Ac) ; c ∈ {R, G, B} in Q0.10 format
 );
     
     // Pipeline registers
@@ -22,6 +22,6 @@ module Multiplier_TE (
         end
     end
     
-    assign product = result_P[13:0]; // Scale down to Q0.14 and eliminate overflow
+    assign product = result_P[13:4]; // Scale down to Q0.10 and eliminate overflow
     
 endmodule
