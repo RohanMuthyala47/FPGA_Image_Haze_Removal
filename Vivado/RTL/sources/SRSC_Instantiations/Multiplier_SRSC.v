d2681a4084c20056856e0504e354fd272216dfcf
Rohan47 <rohanmuthyala1234@gmail.com>
2025-09-23 22:11:01 +0530

    Update Multiplier_SRSC.v

--- Vivado/RTL/sources/SRSC_Instantiations/Multiplier_SRSC.v
+++ Vivado/RTL/sources/SRSC_Instantiations/Multiplier_SRSC.v
@@ -2,13 +2,13 @@ module Multiplier_SRSC (
     input clk, rst,
     
     input  [7:0]  Ic_minus_Ac, // Q8.0
-    input  [9:0] Inv_Trans,   // Q2.10
+    input  [7:0] Inv_Trans,    // Q2.6
     
     output [7:0]  result       // Q8.0
 );
     
-    reg  [7:0] Ic_minus_Ac_P;
-    reg [9:0] Inv_Trans_P;
+    reg [7:0] Ic_minus_Ac_P;
+    reg [7:0] Inv_Trans_P;
     
     always @(posedge clk) begin
         if(rst) begin
@@ -21,12 +21,12 @@ module Multiplier_SRSC (
         end
     end
     
-    // Q8.0 * Q2.8 = Q10.8
-    wire [17:0] mult_result = Ic_minus_Ac_P * Inv_Trans_P;
+    // Q8.0 * Q2.6 = Q10.6
+    wire [15:0] mult_result = Ic_minus_Ac_P * Inv_Trans_P;
     
-    // Eliminate fractional bits
-    wire [9:0] scaled_result = mult_result[17:8];
     // Scale down to 8-bit range
+    wire [9:0] scaled_result = mult_result[15:6];
+    
     assign result = (scaled_result > 8'd255) ? 8'd255 : scaled_result;
 
 endmodule
