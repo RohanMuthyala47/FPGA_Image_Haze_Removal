f1393b787ee84852d0d90a0c0ae349b8e8b6cbf7
Rohan47 <rohanmuthyala1234@gmail.com>
2025-10-18 15:03:44 +0530

    Update Multiplier_SRSC.v

--- Vivado/RTL/sources/SRSC_Instantiations/Multiplier_SRSC.v
+++ Vivado/RTL/sources/SRSC_Instantiations/Multiplier_SRSC.v
@@ -1,32 +1,28 @@
 module Multiplier_SRSC (
-    input clk, rst,
+    input        clk,
     
-    input  [7:0]  Ic_minus_Ac, // Q8.0
-    input  [7:0] Inv_Trans,    // Q2.6
+    input  [7:0] Ic_minus_Ac, // 8-bit value
+    input  [7:0] Inv_Trans,   // Fixed Point Q2.6
     
-    output [7:0]  result       // Q8.0
+    output [7:0] result       // 8-bit result
 );
     
+    // Pipeline Registers
     reg [7:0] Ic_minus_Ac_P;
     reg [7:0] Inv_Trans_P;
     
+    (* use_dsp = "yes" *)
+    wire [15:0] product;
+    
     always @(posedge clk) begin
-        if(rst) begin
-            Ic_minus_Ac_P <= 0;
-            Inv_Trans_P <= 0;
-        end
-        else begin
-            Ic_minus_Ac_P <= Ic_minus_Ac;
-            Inv_Trans_P <= Inv_Trans;
-        end
+        Ic_minus_Ac_P <= Ic_minus_Ac;
+        Inv_Trans_P <= Inv_Trans; 
     end
     
-    // Q8.0 * Q2.6 = Q10.6
-    wire [15:0] mult_result = Ic_minus_Ac_P * Inv_Trans_P;
+    assign product = Ic_minus_Ac_P * Inv_Trans_P;  //Q10.6
     
-    // Scale down to 8-bit range
-    wire [9:0] scaled_result = mult_result[15:6];
+    wire [9:0] scaled_product = product[15:6];
+    // Scale down to 8-bit result
+    assign result = (scaled_product[9:8]) ? 8'd255 : scaled_product[7:0];
     
-    assign result = (scaled_result > 8'd255) ? 8'd255 : scaled_result;
-
 endmodule
