8420089d5baa7ca42db1df040be9a3b231c82432
Rohan47 <rohanmuthyala1234@gmail.com>
2025-10-18 14:59:20 +0530

    Update Multiplier_TE.v

--- Vivado/RTL/sources/TE_Instantiations/Multiplier_TE.v
+++ Vivado/RTL/sources/TE_Instantiations/Multiplier_TE.v
@@ -1,27 +1,27 @@
 // Compute ω * Pc / Ac
 module Multiplier_TE (
-    input         clk,
-    input         rst,
+    input        clk,
     
-    input  [7:0]  Fc,     // Filter result
-    input  [13:0] Inv_Ac, // Scaled Inverted Atmospheric Light value in Q0.12 format (ω * 1/Ac) 
+    input  [7:0] Fc,     // Filter result
+    input  [9:0] Inv_Ac, // Scaled Inverted Atmospheric Light value in Q0.10 format (ω * 1/Ac) 
     
-    output [7:0] product // ω * min(Pc / Ac) ; c ∈ {R, G, B} in Q0.8 format
+    output [9:0] product // ω * min(Pc / Ac) ; c ∈ {R, G, B} in Q0.10 format
 );
     
     // Pipeline registers
-    reg [17:0] result_P;
+    reg [7:0] Fc_P;
+    reg [9:0] Inv_Ac_P;
     
-    // Pipeline inputs to reduce fan-out
+    (* use_dsp = "yes" *)
+    wire [17:0] result;
+    
     always @(posedge clk) begin
-        if(rst) begin
-            result_P <= 0;
-        end
-        else begin
-            result_P <= Fc * Inv_Ac[13:4]; // Q8.10
-        end
+        Fc_P <= Fc;
+        Inv_Ac_P <= Inv_Ac;
     end
     
-    assign product = result_P[9:2]; // Scale down to Q0.8 and eliminate overflow
-    
+    assign result = Fc_P * Inv_Ac_P;    //Q8.10
+    // Scale down to Fixed Point Q0.10
+    assign product = result[9:0];
+            
 endmodule
