dd8e5d2f7bd66176be4588b7c6b97df08f7306c4
Rohan47 <rohanmuthyala1234@gmail.com>
2025-10-20 13:30:15 +0530

    Update ALE.v

--- Vivado/RTL/sources/ALE.v
+++ Vivado/RTL/sources/ALE.v
@@ -1,38 +1,38 @@
 // Atmospheric Light Estimation Module
 `define Image_Size (512 * 512)
 module ALE (
-    input wire        clk, rst,
+    input            clk, rst,
     
-    input wire        input_valid,                                 // Input data valid signal
-    input wire [23:0] input_pixel_1, input_pixel_2, input_pixel_3,
-                      input_pixel_4, input_pixel_5, input_pixel_6,
-                      input_pixel_7, input_pixel_8, input_pixel_9, // 3x3 window input
+    input            input_valid,                                 // Input data valid signal
+    input     [23:0] input_pixel_1, input_pixel_2, input_pixel_3,
+                     input_pixel_4, input_pixel_5, input_pixel_6,
+                     input_pixel_7, input_pixel_8, input_pixel_9, // 3x3 window input
     
-    output reg [7:0]  A_R,
-    output reg [7:0]  A_G,
-    output reg [7:0]  A_B,            // Atmospheric Light Values
+    output reg [7:0] A_R,
+    output reg [7:0] A_G,
+    output reg [7:0] A_B,      // Atmospheric Light Values
     
-    output reg [9:0]  Inv_A_R,
-    output reg [9:0]  Inv_A_G,
-    output reg [9:0]  Inv_A_B,        // Inverse Atmospheric Light Values (Q0.14)
+    output reg [9:0] Inv_A_R,
+    output reg [9:0] Inv_A_G,
+    output reg [9:0] Inv_A_B,  // Inverse Atmospheric Light Values (Q0.14)
     
-    output reg        ALE_done            // Signal to indicate entire image has been processed
+    output reg       ALE_done  // Signal to indicate entire image has been processed
 );
 
-    reg [17:0] PixelCounter;
+    reg [17:0] pixel_counter;
     
     // Keep track of the number of pixels processed through the module
     always @(posedge clk) begin
         if (rst)
-            PixelCounter <= 0;
+            pixel_counter <= 0;
         if (input_valid)
-            PixelCounter <= PixelCounter + 1;
+            pixel_counter <= pixel_counter + 1;
     end
     
     always @(posedge clk) begin
         if (rst)
             ALE_done <= 0;
-        if(PixelCounter == (`Image_Size - 1))
+        if(pixel_counter == (`Image_Size - 1))
             ALE_done <= 1;    // All pixels have been processed through the ALE module
     end
     
@@ -40,12 +40,12 @@ module ALE (
     wire [7:0] minimum_red, minimum_green, minimum_blue;
     
     // Pipeline Registers (Stage 1)
-    reg [7:0] minimum_red_P, minimum_green_P, minimum_blue_P;
+    reg [7:0]  minimum_red_P, minimum_green_P, minimum_blue_P;
     
     always @(posedge clk) begin
-        minimum_red_P <= minimum_red;
+        minimum_red_P   <= minimum_red;
         minimum_green_P <= minimum_green;
-        minimum_blue_P <= minimum_blue;
+        minimum_blue_P  <= minimum_blue;
     end
 
     // Dark channel pixel value
@@ -56,9 +56,9 @@ module ALE (
     
     wire [7:0] Dark_channel_Red, Dark_channel_Green, Dark_channel_Blue;
     
-    assign Dark_channel_Red = (Dark_channel > Dark_channel_P) ? minimum_red_P : A_R;
+    assign Dark_channel_Red   = (Dark_channel > Dark_channel_P) ? minimum_red_P   : A_R;
     assign Dark_channel_Green = (Dark_channel > Dark_channel_P) ? minimum_green_P : A_G;
-    assign Dark_channel_Blue = (Dark_channel > Dark_channel_P) ? minimum_blue_P : A_B;
+    assign Dark_channel_Blue  = (Dark_channel > Dark_channel_P) ? minimum_blue_P  : A_B;
     
     // LUT outputs
     wire [9:0] LUT_Inv_AR, LUT_Inv_AG, LUT_Inv_AB;
@@ -119,19 +119,19 @@ module ALE (
     );
     
     // Look-Up Tables to output the reciprocal of the Atmospheric Light values in Q0.12 format
-    Atmospheric_Light_Reciprocal_LUT Red_Atmospheric_Light_ReciprocalLUT (
+    Atmospheric_Light_Reciprocal_LUT Red_Atmospheric_Light_Reciprocal_LUT (
         .in(Dark_channel_Red),
         
         .out(LUT_Inv_AR)
     );
     
-    Atmospheric_Light_Reciprocal_LUT Green_Atmospheric_Light_ReciprocalLUT (
+    Atmospheric_Light_Reciprocal_LUT Green_Atmospheric_Light_Reciprocal_LUT (
         .in(Dark_channel_Green),
         
         .out(LUT_Inv_AG)
     );
     
-    Atmospheric_Light_Reciprocal_LUT Blue_Atmospheric_Light_ReciprocalLUT (
+    Atmospheric_Light_Reciprocal_LUT Blue_Atmospheric_Light_Reciprocal_LUT (
         .in(Dark_channel_Blue),
         
         .out(LUT_Inv_AB)
