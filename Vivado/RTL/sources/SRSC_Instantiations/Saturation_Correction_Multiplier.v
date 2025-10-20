48cccd9091af64ebb7ccc8a3f9cd8423dc2bc1d5
Rohan47 <rohanmuthyala1234@gmail.com>
2025-10-20 13:25:45 +0530

    Update Saturation_Correction_Multiplier.v

--- Vivado/RTL/sources/SRSC_Instantiations/Saturation_Correction_Multiplier.v
+++ Vivado/RTL/sources/SRSC_Instantiations/Saturation_Correction_Multiplier.v
@@ -1,24 +1,24 @@
-// Compute Ac^β * Jc^(1-β)
+// Compute Ac^0.3 * Jc^0.7
+(* use_dsp = "yes" *)
 module Saturation_Correction_Multiplier (
-    input        clk,
-    input  [9:0] Ac,              // Fixed Point Q3.7 format
-    input  [9:0] Jc,              // Fixed Point Q6.4 format
-    output [7:0] Corrected_Pixel  // Unsigned 8-bit output
+    input         clk,
+    input   [9:0] Ac,       // Q3.7 format
+    input   [9:0] Jc,       // Q6.4 format
+    output  [7:0] Corrected_Pixel    // Unsigned 8-bit output
 );
-
-    // Pipeline Registers
-    reg [9:0] Ac_P, Jc_P;
     
-    (* use_dsp = "yes" *)
-    wire [19:0] product;
+    reg [7:0] Ac_P;
+    reg [9:0] Jc_P;
+    
+    wire [17:0] product;
     
     always @(posedge clk) begin
-        Ac_P <= Ac;
+        Ac_P <= Ac[9:2];
         Jc_P <= Jc;
     end
     
-    assign product = Ac_P * Jc_P;
+    assign product = Ac_P * Jc_P; //Q9.9
     // Scale down to 8 bit value
-    assign Corrected_Pixel = (product[19]) ? 8'd255 : product[18:11];
+    assign Corrected_Pixel = (product[17]) ? 8'd255 : product[16:9];
     
 endmodule
