637d0cae3bea1b7904f656f0959cebdec1bd12c3
Rohan47 <rohanmuthyala1234@gmail.com>
2025-10-20 13:19:48 +0530

    Update Subtractor_SRSC.v

--- Vivado/RTL/sources/SRSC_Instantiations/Subtractor_SRSC.v
+++ Vivado/RTL/sources/SRSC_Instantiations/Subtractor_SRSC.v
@@ -2,14 +2,14 @@ module Subtractor_SRSC (
     input [7:0]  Ic,
     input [7:0]  Ac, 
     
-    output [7:0] Ic_minus_Ac,   // |Ic - Ac|
-    output       add_or_sub     // Add with or subtract from Atmospheric Light in the Adder module
+    output [7:0] Ic_minus_Ac,  // |Ic - Ac|
+    output       add_or_sub    // Add with or subtract from Atmospheric Light in the Adder module
 );
     
     localparam ADD = 1, SUB = 0;
     
     assign Ic_minus_Ac = (Ic > Ac) ? (Ic - Ac) : (Ac - Ic);
     
-    assign add_or_sub = (Ic > Ac) ? ADD : SUB;
+    assign add_or_sub  = (Ic > Ac) ? ADD : SUB;
     
 endmodule
