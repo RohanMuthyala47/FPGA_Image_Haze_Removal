20bd6a7ec99dc2bce48e3781d40bbbbb1e12b91d
Rohan47 <rohanmuthyala1234@gmail.com>
2025-10-20 13:13:10 +0530

    Update WindowFilter.v

--- Vivado/RTL/sources/TE_Instantiations/WindowFilter.v
+++ Vivado/RTL/sources/TE_Instantiations/WindowFilter.v
@@ -1,25 +1,33 @@
 module WindowFilter (
     input        w_corner, w_edge, w_center,
+    
     input  [7:0] input_pixel_1, input_pixel_2, input_pixel_3,
                  input_pixel_4, input_pixel_5, input_pixel_6,
                  input_pixel_7, input_pixel_8, input_pixel_9,
+    
     output [7:0] filtered_pixel
 );
 
-    wire [11:0] sum1, sum2, sum3;
+    wire [8:0] sum1, sum2, sum3, sum4;
+    
+    wire [10:0] corner_pixels_sum, edge_pixels_sum;
+    
     wire [12:0] sum;
-    wire [9:0]  mean_sum;
 
     // Weighted sums
-    assign sum1 = (input_pixel_1 << w_corner) + (input_pixel_2 << w_edge) + (input_pixel_3 << w_corner);
-    assign sum2 = (input_pixel_4 << w_edge) + (input_pixel_5 << (w_center << 1)) + (input_pixel_6 << w_edge);
-    assign sum3 = (input_pixel_7 << w_corner) + (input_pixel_8 << w_edge) + (input_pixel_9 << w_corner);
-
-    assign sum = sum1 + sum2 + sum3;
+    assign sum1 = input_pixel_1 + input_pixel_3;
+    assign sum2 = input_pixel_7 + input_pixel_9;
     
-    assign mean_sum = sum >> 3;
+    assign corner_pixels_sum = (sum1 + sum2) << w_corner;
+    
+    assign sum3 = input_pixel_2 + input_pixel_4;
+    assign sum4 = input_pixel_6 + input_pixel_8;
+    
+    assign edge_pixels_sum = (sum3 + sum4) << w_edge;
+    
+    assign sum = corner_pixels_sum + edge_pixels_sum + (input_pixel_5 << (w_center << 1));
 
     // Normalization
-    assign filtered_pixel = (w_center) ? (mean_sum >> 1) : ((mean_sum) - (mean_sum >> 3));
+    assign filtered_pixel = (w_center) ? (sum >> 4) : ((sum[12:3]) - (sum[12:6]));
 
 endmodule
