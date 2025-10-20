a81135abf4b493390a43b9e800f6e782f7261c71
Rohan47 <rohanmuthyala1234@gmail.com>
2025-10-20 13:11:49 +0530

    Update FilterWeights_Estimation.v

--- Vivado/RTL/sources/TE_Instantiations/FilterWeights_Estimation.v
+++ Vivado/RTL/sources/TE_Instantiations/FilterWeights_Estimation.v
@@ -1,30 +1,19 @@
 module FilterWeights_Estimation (
-    input  [7:0] input_pixel_1, input_pixel_2, input_pixel_3,
-                 input_pixel_4,                input_pixel_6,
-                 input_pixel_7, input_pixel_8, input_pixel_9,
+    input [7:0] input_pixel_1, input_pixel_2, input_pixel_3,
+                input_pixel_4,                input_pixel_6,
+                input_pixel_7, input_pixel_8, input_pixel_9,
     
-    output       w_corner, w_edge, w_center
+    output      w_corner, w_edge, w_center
 );
     
-    parameter  THRESHOLD = 80; // Threshold Value for Edge Detection
+    parameter THRESHOLD = 8'd80; // Threshold Value to detect edges
     
-    wire [7:0] diagonal1, diagonal2, horizontal, vertical;
-    wire       d_edge_detected, h_v_edge_detected;
+    assign w_corner = ((input_pixel_1 > input_pixel_9 ? input_pixel_1 - input_pixel_9 : input_pixel_9 - input_pixel_1) >= THRESHOLD) |
+                      ((input_pixel_3 > input_pixel_7 ? input_pixel_3 - input_pixel_7 : input_pixel_7 - input_pixel_3) >= THRESHOLD);
     
-    // Calculate absolute differences for edge detection
-    assign diagonal1  = (input_pixel_1 > input_pixel_9) ? (input_pixel_1 - input_pixel_9) : (input_pixel_9 - input_pixel_1);
-    assign diagonal2  = (input_pixel_3 > input_pixel_7) ? (input_pixel_3 - input_pixel_7) : (input_pixel_7 - input_pixel_3);
-    assign horizontal = (input_pixel_4 > input_pixel_6) ? (input_pixel_4 - input_pixel_6) : (input_pixel_6 - input_pixel_4);
-    assign vertical   = (input_pixel_2 > input_pixel_8) ? (input_pixel_2 - input_pixel_8) : (input_pixel_8 - input_pixel_2);
+    assign w_edge   = ((input_pixel_4 > input_pixel_6 ? input_pixel_4 - input_pixel_6 : input_pixel_6 - input_pixel_4) >= THRESHOLD) |
+                      ((input_pixel_2 > input_pixel_8 ? input_pixel_2 - input_pixel_8 : input_pixel_8 - input_pixel_2) >= THRESHOLD);
     
-    // Check if diagonal or vertical/horizontal edges exceed threshold
-    assign d_edge_detected   = (diagonal1 >= THRESHOLD) || (diagonal2 >= THRESHOLD);
-    assign h_v_edge_detected = (horizontal >= THRESHOLD) || (vertical >= THRESHOLD);
-    
-    assign w_corner = d_edge_detected;
-                      
-    assign w_edge = h_v_edge_detected;
-                      
-    assign w_center = (d_edge_detected | h_v_edge_detected);
+    assign w_center = w_corner | w_edge;
     
 endmodule
