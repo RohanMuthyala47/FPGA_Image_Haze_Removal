03a8e370391a6c10947470c71c4afca3037d3885
Rohan47 <rohanmuthyala1234@gmail.com>
2025-10-24 22:13:28 +0530

    Update Adder_SRSC.v

--- Vivado/RTL/sources/SRSC_Instantiations/Adder_SRSC.v
+++ Vivado/RTL/sources/SRSC_Instantiations/Adder_SRSC.v
@@ -1,14 +1,21 @@
 module Adder_SRSC (
-    input [7:0]  Ac,
-    input [7:0]  Ic,
-    input [7:0]  Multiplier_out,
+    input  [7:0] Ac,
+    input  [7:0] Ic,
+    input  [7:0] Multiplier_out,
     
     input        add_or_sub,
-
+    
     output [7:0] sum
 );
-    
-    assign sum = add_or_sub ? (Ic > Ac) ? (Ac - Multiplier_out) : (Ac + Multiplier_out) :
-                                          (Ac > Multiplier_out) ? (Ac - Multiplier_out) : 0;
 
+    wire ic_gt_ac  = (Ic > Ac);
+    wire ac_gt_mul = (Ac > Multiplier_out);
+
+    wire [7:0] add_res = Ac + Multiplier_out;
+    wire [7:0] sub_res = Ac - Multiplier_out;
+
+    assign sum = add_or_sub ? 
+                 (ic_gt_ac ? sub_res : add_res) :
+                 (ac_gt_mul ? sub_res : 8'd0);
+    
 endmodule
